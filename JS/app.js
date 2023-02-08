d813c24aa8cf5d9fde64629d86469f9f36464013
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-08 18:33:43 +0100

    END palindrome

---
+++ JS/app.js
@@ -0,0 +1,53 @@
+// // CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
+// let wordInsert = prompt('Inserisci una parola!')
+// console.log(wordInsert)
+
+// let word = wordInsert.toLocaleLowerCase()
+// console.log(word)
+
+
+// // .CREARE UNA NUOVA VARIABILE CON LA PAROLA AL CONTRARIO    
+// let reverseWord = revertedWord(word)
+
+
+
+// // .CONFRONTARE LE 2 STRINGHE
+
+// if (word === reverseWord) {
+//     console.log('La parola', word, 'è palindroma')
+// }
+// else {
+//     console.log('La parola', word, 'non è palindroma')
+// }
+
+
+
+// // .FUNZIONI
+
+// function revertedWord(parolaReverse) {
+//     let reverseWord = ''
+
+//     // .IMPOSTARE UN CICLO CHE PRENDE L'ULTIMA LETTERA E LA PORTA ALL'INIZIO E COSI VIA
+
+//     for (let i = parolaReverse.length - 1; i >= 0; i--) {
+//         reverseWord += parolaReverse[i]
+//     }
+
+//     // console.log(reverseWord)
+//     return reverseWord
+// }
+
+
+// // // // // // // // // // // // // // // // // // // // // // // PARI E DISPARI
+
+
+// chiedere all'utente di scegliere pari o dispari e di inserire un numero
+let scelta = prompt('Seleziona pari o dispari!')
+
+if (scelta === 'pari'){
+    
+}
+
+// generare un numero random per il pc usando una funzione
+// sommiamo i 2 numeri
+// stabiliamo se il risultato è pari o dispari con una funzione
